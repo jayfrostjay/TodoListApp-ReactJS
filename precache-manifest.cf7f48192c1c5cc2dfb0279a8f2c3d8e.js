ba1927a1c7f78efcfd7c73b78e98ddcdec039949
Jayson Garcia <jayfrostgarcia@gmail.com>
2020-03-29 16:52:01 +0800

    Updates

---
+++ precache-manifest.cf7f48192c1c5cc2dfb0279a8f2c3d8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24702104d87639a36d1baedb0acf3d11",
+    "url": "/TodoListApp-ReactJS/index.html"
+  },
+  {
+    "revision": "bdc91ce46e547373319e",
+    "url": "/TodoListApp-ReactJS/static/css/main.49bfe398.chunk.css"
+  },
+  {
+    "revision": "c099c76bc6e99e07e201",
+    "url": "/TodoListApp-ReactJS/static/js/2.12e29ac5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TodoListApp-ReactJS/static/js/2.12e29ac5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdc91ce46e547373319e",
+    "url": "/TodoListApp-ReactJS/static/js/main.99f65a39.chunk.js"
+  },
+  {
+    "revision": "7ab8471d133516f41164",
+    "url": "/TodoListApp-ReactJS/static/js/runtime-main.df73e9fb.js"
+  }
+]);
